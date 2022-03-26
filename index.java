7b525e249f762f63d6e3aae0de7c3ee818d3176c
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-03-25 21:43:47 -0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,43 @@
+import java.util.*;  
+
+
+class Main {
+  public static void main(String[] args) {
+  double a, b;
+    String operacaoStr;
+    char operacao;
+    Scanner entrada = new Scanner (System.in);
+
+    System.out.println( "<Adição>");
+     System.out.println("<Subtração>");
+     System.out.println("<MultiplixaÃ§Ã£o>");
+     System.out.println("<Divisão>");
+     System.out.println("Escolha a operaÃ§Ã£o");
+
+    operacaoStr = entrada .next();
+    operacao = operacaoStr.charAt(0);
+     System.out.println("1o operando: ");
+    a = entrada.nextDouble();
+     System.out.println ("2o operando : ");
+    b = entrada .nextDouble();
+
+    switch(operacao){
+    case '+': 
+        System.out.println("Soma =" + (a+b));
+        break;
+    case '-': 
+        System.out.println("Subtração =" + (a-b));
+        break;
+      case '*' : 
+        System.out.println("Multiplicação = " + (a*b));
+        break;
+      case '/': 
+        System.out.println("Divisão =" + (a/b) );
+        break;
+        default: 
+        System.out.println ("Operação inválida");
+      }
+
+  }
+   
+  }
